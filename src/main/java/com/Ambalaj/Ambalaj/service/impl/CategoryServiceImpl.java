004f91d7f8d420c5203787787d9a7f59ec4b5351
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2024-08-20 16:21:38 +0300

    use base service with CategoryServiceImpl

--- src/main/java/com/Ambalaj/Ambalaj/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/Ambalaj/Ambalaj/service/impl/CategoryServiceImpl.java
@@ -1,30 +1,38 @@
 package com.Ambalaj.Ambalaj.service.impl;
 
-import com.Ambalaj.Ambalaj.exception.NotFoundException;
 import com.Ambalaj.Ambalaj.model.CategoryEntity;
-import com.Ambalaj.Ambalaj.model.IndustryEntity;
 import com.Ambalaj.Ambalaj.repository.CategoryRepository;
 import com.Ambalaj.Ambalaj.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class CategoryServiceImpl implements CategoryService {
+public class CategoryServiceImpl extends BaseServiceImpl<CategoryEntity, Long> implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Override
+    public JpaRepository<CategoryEntity, Long> getRepository() {
+        return categoryRepository;
+    }
+
+    @Override
     public List<CategoryEntity> getCategoriesByIds(List<Long> categoryIds) {
-        List<CategoryEntity> categories = categoryRepository.findAllById(categoryIds);
-        Set<Long> foundIds = categories.stream().map(CategoryEntity::getId).collect(Collectors.toSet());
-        List<Long> missingIds = categoryIds.stream().filter(id -> !foundIds.contains(id)).toList();
-        if (!missingIds.isEmpty()) {
-            throw new NotFoundException("Categories not found for IDs: " + missingIds);
-        }
-        return categories;
+        return getEntitiesByIds(categoryIds, "Categories");
+    }
+
+    @Override
+    public Long getId(CategoryEntity categoryEntity) {
+        return categoryEntity.getId();
+    }
+
+    @Override
+    protected void updateEntityFields(CategoryEntity existingEntity, CategoryEntity updatedEntity) {
+
     }
 }
+
+
