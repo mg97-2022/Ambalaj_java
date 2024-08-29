09c3dcbea72662cdeee9bddafe45decdda3e1f9f
mg97-2022 <mohamedgamalsaied1997@gmail.com>
2024-08-29 16:22:13 +0300

    sql scripts to run after project start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into countries (name) values ('Egypt'), ('Libya'), ('Sudan'), ('Turkey');
+insert into cities (name, country_id) values ('Alexandria', 1);
+insert into industries (name) values ('industry 1'), ('industry 2');
+
+INSERT INTO users (id, enabled, locked, app_register_method, email, password, type)
+VALUES ('46e29bb7-37f9-4d2e-ba1d-6199cf4b37f7', true, false, 'EMAIL', 'admin@ambalaj.com', '$2a$10$X3OCsW0CqGFlod6rNMYq5euCDRyWRtuWx5gHwz0Z7/E8DgDKTpZxy', 'ADMIN');
+
+INSERT INTO admins(user_id, first_name, last_name, phone_number, role)
+VALUES ('46e29bb7-37f9-4d2e-ba1d-6199cf4b37f7', 'Mohamed', 'Gamal', '01000000', 'ADMIN');
